70922aaf49a3955fd45531e4f8a76bc29d465d1d
unknown <sanelemngadi17@gmail.com>
2023-07-17 13:01:40 +0200

    configure zeet

--- nma/settings.py
+++ nma/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)
 
-ALLOWED_HOSTS = ['.vercel.app','127.0.0.1', '.now.sh']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
--- store/models.py
+++ store/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+# from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -6,13 +7,28 @@ from django.db import models
 #     name = models.CharField(max_length=300)
 #     product = models.ForeignKey(Product)
 
+# class ReviewReactions(models.Model):
+#     sharp = models.BooleanField()
+#     not_sharp = models.BooleanField()
+#     user = models.ForeignKey(User, on_delete=models.CASCADE)
+#     product = models.ForeignKey(Product)
+
+#many to many relationship for product
+# class ProductReview(models.Model):
+#     heading = models.CharField("Review Heading", max_length=100)
+#     name = models.CharField("Reviewer", max_length=30)
+#     date_reviewed = models.DateTimeField(auto_now_add=True)
+#     review = models.TextField()
+#     reactions = models.ManyToManyField(ReviewReactions)
+
+
 # class Category(models.Model):
 #     name = models.CharField('Product Category Name')
 
-# class Filters(models.Model):
+# class Filter(models.Model):
 #     name = models.CharField(max_length=50)
 
-# class Products(models.Model):
+# class Product(models.Model):
 #     name = models.CharField(max_length=50)
 #     description = models.TextField()
 #     available = models.BooleanField()
